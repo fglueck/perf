ffcacbb07896de525deabd3001e6c9c230ee3123
Frank Glück <glueck@dozent.net>
2025-01-16 11:51:25 +0100

    Update perf.php

--- perf.php
+++ perf.php
@@ -1,8 +1,18 @@
 <?php
-
+/**
+Author Frank Glück
+https://github.com/fglueck/perf
+https://github.com/fglueck/PHP-Performance
+*/
 class perf {
-    private $times = [];
-    private $start = null;
+    private array $times = [];
+    private float $start = 0.0;
+    
+    private int   $loops = 1000;
+    
+    public function __construct(int $loops=1000) {
+        $this->loops = $loops;
+    }
 
     public function start() {
         $this->start = microtime(true);
@@ -17,5 +27,12 @@ class perf {
             $time = number_format($time, 5);
             echo sprintf("%s%% %ss %s\n", number_format(bcmul(bcdiv($time,$bench,10),100, 10),1), $time, $label);
         }
+    }
+    public function test(string $label, callable $call) {
+        $this->start();
+        for($i=0;$i<$this->loops;$i++) {
+            $call();
+        }
+        $this->stop($label);
     }
 }
