a6089677836aaccbc714bfd2f22ab1fc3b7115b2
Frank Glück <glueck@dozent.net>
2025-01-16 16:25:58 +0100

    Update perf.php

--- perf.php
+++ perf.php
@@ -20,13 +20,14 @@ class perf {
     public function stop(string $label) {
         $this->times[$label] = microtime(true) - $this->start;
     }
-    public function report() {
+    public function report(): string {
         asort($this->times, SORT_NUMERIC);
         $bench = current($this->times);
         foreach($this->times as $label => $time) {
-            $time = number_format($time, 5);
-            echo sprintf("%s%% %ss %s\n", number_format(bcmul(bcdiv($time,$bench,10),100, 10),1), $time, $label);
+            #$time = number_format($time, 5);
+            $out[] = sprintf("%0.1f%% %0.5fs %s\n", bcmul(bcdiv(sprintf('%1.30f',$time),sprintf('%1.30f', $bench),10),100, 10), $time, $label);
         }
+        return implode("\n", $out);
     }
     public function test(string $label, callable $call) {
         $this->start();
